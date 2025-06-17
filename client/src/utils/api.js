24ba0fdadd9d22eb8e4a13b66a0b8737d2f7f1ba
Systech Consultancy <dev@systechconsultancy.in>
2025-06-17 08:19:46 +0530

    Fix API base URL reference in api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL
+const API_BASE = import.meta.env.VITE_API_BASE_URL
 
 export const initiateBooking = async (studentData) => {
   try {
