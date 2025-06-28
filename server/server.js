6d19246361c348c367758c24fd1e9f0e7ed24a3d
Systech Consultancy <dev@systechconsultancy.in>
2025-06-28 11:49:26 +0530

    fix: Update root route response to return 404 error message instead of testing message

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use("/api/payment-qr", qrRoutes);
 
 
 app.get('/', (req, res) => {
-    res.send('Testing');
+    res.send('ERROR 404 : URL NOT FOUND');
 });
 
 const PORT = process.env.PORT || 5000;
