7e6f9ee7e4f2a24d88f958ae47da99e301385413
Systech Consultancy <dev@systechconsultancy.in>
2025-07-03 09:36:40 +0530

    feat: Integrate Vercel Speed Insights into the App component and update package dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import AppRoutes from "./routes/AppRoutes"
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   <>
     <AppRoutes />
     <Analytics />
+    <SpeedInsights/>
   </>
   );
 }
